b17c6863c0e1cc26a0a97d6c6dc6322734aeeddb
Kiselev Kostya <kiselev.k0101@gmail.com>
2026-03-26 15:07:35 +0500

    Реализован вывод книг

--- LibraryProject/FormBooks.Designer.cs
+++ LibraryProject/FormBooks.Designer.cs
@@ -28,22 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnLogout = new Button();
+            lblUsername = new Label();
+            dgvBooks = new DataGridView();
+            panel1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBooks).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.AliceBlue;
+            panel1.Controls.Add(flowLayoutPanel2);
+            panel1.Controls.Add(flowLayoutPanel1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1099, 60);
+            panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Dock = DockStyle.Left;
+            flowLayoutPanel2.Location = new Point(0, 0);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Padding = new Padding(10);
+            flowLayoutPanel2.Size = new Size(566, 60);
+            flowLayoutPanel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(btnLogout);
+            flowLayoutPanel1.Controls.Add(lblUsername);
+            flowLayoutPanel1.Dock = DockStyle.Right;
+            flowLayoutPanel1.Location = new Point(572, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(10);
+            flowLayoutPanel1.RightToLeft = RightToLeft.Yes;
+            flowLayoutPanel1.Size = new Size(527, 60);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Location = new Point(393, 13);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(111, 32);
+            btnLogout.TabIndex = 0;
+            btnLogout.Text = "Выход";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += BtnLogout_Click;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(334, 10);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(53, 21);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "label1";
+            // 
+            // dgvBooks
+            // 
+            dgvBooks.AllowUserToAddRows = false;
+            dgvBooks.AllowUserToDeleteRows = false;
+            dgvBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvBooks.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvBooks.BackgroundColor = Color.White;
+            dgvBooks.BorderStyle = BorderStyle.None;
+            dgvBooks.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvBooks.ColumnHeadersVisible = false;
+            dgvBooks.Dock = DockStyle.Fill;
+            dgvBooks.Location = new Point(0, 60);
+            dgvBooks.MultiSelect = false;
+            dgvBooks.Name = "dgvBooks";
+            dgvBooks.ReadOnly = true;
+            dgvBooks.RowHeadersVisible = false;
+            dgvBooks.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvBooks.Size = new Size(1099, 553);
+            dgvBooks.TabIndex = 1;
             // 
             // FormBooks
             // 
             AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(912, 552);
+            ClientSize = new Size(1099, 613);
+            Controls.Add(dgvBooks);
+            Controls.Add(panel1);
             Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             Name = "FormBooks";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Список книг";
+            panel1.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBooks).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnLogout;
+        private Label lblUsername;
+        private DataGridView dgvBooks;
     }
 }
--- LibraryProject/FormBooks.cs
+++ LibraryProject/FormBooks.cs
@@ -1,4 +1,6 @@
 ﻿using LibraryProject.Models;
+using LibraryProject.Properties;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +15,110 @@ namespace LibraryProject
 {
     public partial class FormBooks : Form
     {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
         public FormBooks(User currentUser, bool isGuest)
         {
             InitializeComponent();
+            CurrentUser = currentUser;
+            IsGuest = isGuest;
 
+            lblUsername.Text = IsGuest ? "Гость" : $"{CurrentUser.Surname} {CurrentUser.Name} {CurrentUser.Patronymic}";
+
+            var colId = new DataGridViewTextBoxColumn();
+            colId.Name = "colId";
+            colId.Visible = false;
+            colId.ReadOnly = true;
+
+            var colPhoto = new DataGridViewImageColumn();
+            colPhoto.Name = "colPhoto";
+            colPhoto.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            colPhoto.Width = 200;
+            colPhoto.FillWeight = 20;
+
+            var colInfo = new DataGridViewTextBoxColumn();
+            colInfo.Name = "colInfo";
+            colInfo.FillWeight = 60;
+            colInfo.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+            dgvBooks.Columns.AddRange([
+                colId, colPhoto, colInfo
+            ]);
+
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            try
+            {
+                using (var db = new LibraryContext())
+                {
+                    var books = db.Books
+                        .Include(i => i.IdAuthorNavigation)
+                        .Include(i => i.IdGenreNavigation)
+                        .Include(i => i.IdPublisherNavigation)
+                        .ToList();
+
+                    dgvBooks.SuspendLayout();
+                    dgvBooks.Rows.Clear();
+
+                    foreach (var book in books)
+                    {
+                        int rowIndex = dgvBooks.Rows.Add();
+                        var row = dgvBooks.Rows[rowIndex];
+
+                        row.Cells["colId"].Value = book.Id;
+                        row.Cells["colPhoto"].Value = Resources.book_placeholder;
+                        row.Cells["colInfo"].Value = FormatBookInfo(book);
+
+                        ApplyRowStyles(row, book);
+                    }
+
+                    dgvBooks.ResumeLayout();
+                    dgvBooks.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+
+        private string FormatBookInfo(Book book)
+        {
+            //throw new NotImplementedException();
+
+            return $"ISBN: {book.Isbn}" + Environment.NewLine +
+                $"Название книги: {book.BookName}" + Environment.NewLine +
+                $"Автор: {book.IdAuthorNavigation.AuthorName}" + Environment.NewLine +
+                $"Жанр: {book.IdGenreNavigation.GenreName}" + Environment.NewLine +
+                $"Издательство: {book.IdPublisherNavigation.PublisherName}" + Environment.NewLine +
+                $"Кол-во страниц: {book.PageCount}" + Environment.NewLine +
+                $"Всего экземпляров: {book.TotalAmount}" + Environment.NewLine +
+                $"Доступно экземпляров: {book.AvailableAmount}" + Environment.NewLine +
+                $"Аннотация: {book.Annotation}" + Environment.NewLine +
+                $"Год издания: {book.PublishYear}" + Environment.NewLine;
+        }
+
+        private void ApplyRowStyles(DataGridViewRow row, Book book)
+        {
+            if (book.AvailableAmount <= 0)
+            {
+                row.DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#FFCCCC");
+            }
+
+            if (book.AvailableAmount >= 1 && book.AvailableAmount <= 2)
+            {
+                row.DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#FFF3CD");
+            }
+        }
+
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
         }
     }
 }
