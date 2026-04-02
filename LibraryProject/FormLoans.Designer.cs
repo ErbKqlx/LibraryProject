b1c1d7b57ddcd9ada1ff3105de93f6edef69f814
Artem <aruporoff@yandex.ru>
2026-04-02 11:24:44 +0500

    ssss

--- LibraryProject/FormLoans.Designer.cs
+++ LibraryProject/FormLoans.Designer.cs
@@ -28,20 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            flowLayoutPanelButtons = new FlowLayoutPanel();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnLogout = new Button();
+            lblUsername = new Label();
+            dgvLoans = new DataGridView();
+            panel1.SuspendLayout();
+            flowLayoutPanelButtons.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvLoans).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.AliceBlue;
+            panel1.Controls.Add(flowLayoutPanelButtons);
+            panel1.Controls.Add(flowLayoutPanel1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1099, 59);
+            panel1.TabIndex = 1;
+            // 
+            // flowLayoutPanelButtons
+            // 
+            flowLayoutPanelButtons.Controls.Add(btnAdd);
+            flowLayoutPanelButtons.Controls.Add(btnEdit);
+            flowLayoutPanelButtons.Controls.Add(btnDelete);
+            flowLayoutPanelButtons.Dock = DockStyle.Left;
+            flowLayoutPanelButtons.Location = new Point(0, 0);
+            flowLayoutPanelButtons.Name = "flowLayoutPanelButtons";
+            flowLayoutPanelButtons.Padding = new Padding(10);
+            flowLayoutPanelButtons.Size = new Size(609, 59);
+            flowLayoutPanelButtons.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.FromArgb(74, 111, 165);
+            btnAdd.FlatAppearance.BorderSize = 0;
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.Location = new Point(13, 13);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(111, 32);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Добавить";
+            btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            btnEdit.BackColor = Color.FromArgb(74, 111, 165);
+            btnEdit.FlatAppearance.BorderSize = 0;
+            btnEdit.FlatStyle = FlatStyle.Flat;
+            btnEdit.Location = new Point(130, 13);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(154, 32);
+            btnEdit.TabIndex = 2;
+            btnEdit.Text = "Редактировать";
+            btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.FromArgb(74, 111, 165);
+            btnDelete.FlatAppearance.BorderSize = 0;
+            btnDelete.FlatStyle = FlatStyle.Flat;
+            btnDelete.Location = new Point(290, 13);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(111, 32);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Удалить";
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(btnLogout);
+            flowLayoutPanel1.Controls.Add(lblUsername);
+            flowLayoutPanel1.Dock = DockStyle.Right;
+            flowLayoutPanel1.Location = new Point(615, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(10);
+            flowLayoutPanel1.RightToLeft = RightToLeft.Yes;
+            flowLayoutPanel1.Size = new Size(484, 59);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Location = new Point(350, 13);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(111, 32);
+            btnLogout.TabIndex = 0;
+            btnLogout.Text = "Назад";
+            btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(291, 10);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(53, 21);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "label1";
+            // 
+            // dgvLoans
+            // 
+            dgvLoans.AllowUserToAddRows = false;
+            dgvLoans.AllowUserToDeleteRows = false;
+            dgvLoans.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvLoans.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvLoans.BackgroundColor = Color.White;
+            dgvLoans.BorderStyle = BorderStyle.None;
+            dgvLoans.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLoans.ColumnHeadersVisible = false;
+            dgvLoans.Dock = DockStyle.Fill;
+            dgvLoans.Location = new Point(0, 59);
+            dgvLoans.MultiSelect = false;
+            dgvLoans.Name = "dgvLoans";
+            dgvLoans.ReadOnly = true;
+            dgvLoans.RowHeadersVisible = false;
+            dgvLoans.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvLoans.Size = new Size(1099, 554);
+            dgvLoans.TabIndex = 2;
             // 
             // FormLoans
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1099, 613);
+            Controls.Add(dgvLoans);
+            Controls.Add(panel1);
+            Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "FormLoans";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Выдачи книг";
+            panel1.ResumeLayout(false);
+            flowLayoutPanelButtons.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvLoans).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private FlowLayoutPanel flowLayoutPanelButtons;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnLogout;
+        private Label lblUsername;
+        private DataGridView dgvLoans;
     }
 }
